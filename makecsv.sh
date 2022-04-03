6215df184e32edd9ae0b7d6c3db676d7d2ce59f7
Devamitta <rusdevamitta@gmail.com>
2022-04-03 16:08:16 +0800

    nice bash

--- makecsv.sh
+++ makecsv.sh
@@ -1,3 +1,14 @@
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "making csv with bold"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
 python3 ods-to-csv-headers.py "Pātimokkha Word by Word.ods" Sheet1 20
 
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "filtering words that have been done"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
 python3 "patimokkha filter.py"
+
+echo "process completed"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
