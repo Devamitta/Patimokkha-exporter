0b53ea2de2fb3fc8be34f828b04d93b9c6eb897e
Devamitta <rusdevamitta@gmail.com>
2022-05-07 22:13:03 +0800

    feedback

--- patimokkha filter.py
+++ patimokkha filter.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv("Pātimokkha Word by Word.csv", sep="\t", dtype= str)
 df.fillna("", inplace=True)
 
-# filter all SBS words
+# filter all covered words
 test1 = df['meaning'] != ""
 test2 = df['sentence'] != ""
 test3 = df['#'] == "1"
@@ -20,6 +20,7 @@ df = df[['bhikkhupātimokkhapāḷi', 'pos', 'grammar', '+case', 'tamil',
         'meaning', 'lit. meaning', 'root', 'rt gp', 'sign', 'base', 
         'construction', 'compound type', 'compound construction', 'abbrev', 
         'source', 'sentence', 'aṭṭhakathā']]
+df.insert(18, 'feedback', None)        
 
 # save csv
 df.to_csv("Pātimokkha Word by Word.csv", sep="\t", index=None)
